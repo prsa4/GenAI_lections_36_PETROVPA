869b893cf83c948c0085cd7a80456d07025bb430
prsa4 <pasapetrov70@gmail.com>
2026-09-09 14:55:39 +0300

    Добавлен обработчик CSV
    Добавлены чтение, фильтрация и статистика CSV-файлов.

---
+++ 3_1_LLM_agent/llm_agent/tool_csvprocessor.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+class CSVProcessor_tool:
+
+    name = "csvprocessor"
+    description = (
+        "Читает CSV-файл, фильтрует его строки ."
+        "И Возвращает статистику по данным."
+    )
+
+    def use(self, file_path, operation="read", col=None, val= None ):
+        df = pd.read_csv(file_path)
+
+        if operation == "read":
+            return df.head().to_string(index=False)
+        
+        
+        elif operation == "filter":
+            filtered = df[df[col] == val]
+            return filtered.to_string(index=False)
+        
+        elif operation == "stats":
+            stats = df[col].describe()
+            return stats.to_string()
+
+
+
